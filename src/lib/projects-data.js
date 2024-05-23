8f285183c78529fb611d6b33a26a8adb77e746d6
Denis Pianelli <denis.pianelli@gmail.com>
2024-05-23 13:24:09 +0200

    chore: Update project descriptions for internationalization support

--- src/lib/projects-data.js
+++ src/lib/projects-data.js
@@ -1,34 +1,40 @@
-const projects = [
+export const projects = [
   {
     id: 1,
     type: 'App',
-		tech: ['Next.js', 'React', 'Prisma', 'Tailwind'],
+    tech: ['Next.js', 'React', 'Prisma', 'Tailwind'],
     title: 'Next.js Auth Starter',
-    description: 'Next.js Auth Starter is a full-stack web application template designed to kickstart your authentication-related projects. It provides a solid foundation for implementing authentication features such as user registration, login, and authentication persistence.',
+    descriptionEn:
+      'Next.js Auth Starter is a full-stack web application template designed to kickstart your authentication-related projects. It provides a solid foundation for implementing authentication features such as user registration, login, and authentication persistence.',
+		descriptionFr: 'Next.js Auth Starter est un modèle d\'application web full-stack conçu pour démarrer vos projets liés à l\'authentification. Il fournit une base solide pour implémenter des fonctionnalités d\'authentification telles que l\'enregistrement d\'utilisateurs, la connexion et la persistance de l\'authentification.,',
     image: '/projects/nextjs-auth-starter.png',
-		codeLink: 'https://github.com/denispianelli/nextjs-tailwind-prisma-nextauth-template',
-		demoLink: 'https://nextjs-tailwind-prisma-nextauth-template.vercel.app/',
+    codeLink:
+      'https://github.com/denispianelli/nextjs-tailwind-prisma-nextauth-template',
+    demoLink: 'https://nextjs-tailwind-prisma-nextauth-template.vercel.app/',
   },
-	{
-		id: 2,
+  {
+    id: 2,
     type: 'App',
-		tech: ['HTML', 'CSS', 'JavaScript'],
+    tech: ['HTML', 'CSS', 'JavaScript'],
     title: 'Todo App',
-    description: 'A simple and intuitive Todo application that allows users to keep track of their tasks. Users can add, delete, and mark tasks as completed. The app also features a clean and responsive design for an optimal user experience.',
+    descriptionEn:
+      'A simple and intuitive Todo application that allows users to keep track of their tasks. Users can add, delete, and mark tasks as completed. The app also features a clean and responsive design for an optimal user experience.',
+		descriptionFr: 'Une application Todo simple et intuitive qui permet aux utilisateurs de suivre leurs tâches. Les utilisateurs peuvent ajouter, supprimer et marquer les tâches comme terminées. L\'application dispose également d\'un design propre et réactif pour une expérience utilisateur optimale.',
     image: '/projects/todo-app.png',
-		codeLink: 'https://github.com/denis-pianelli/interactivity-todo-app',
-		demoLink: 'https://denis-pianelli.github.io/interactivity-todo-app/',
-	},
-	{
-		id: 3,
-		type: 'Game',
-		tech: ['React', 'Redux', 'Sass', 'Phaser', 'Node.js', 'Express'],
-		title: 'O\'Survivors',
-		description: 'O\'Survivors is a 2D multiplayer survival game where players must withstand increasingly powerful waves of monsters. ',
-		image: '/projects/o-survivors.png',
-		codeLink: 'https://github.com/denispianelli/projet-tp-front',
-		demoLink: 'https://projet-tp-front.vercel.app/',
-	}
+    codeLink: 'https://github.com/denis-pianelli/interactivity-todo-app',
+    demoLink: 'https://denis-pianelli.github.io/interactivity-todo-app/',
+  },
+  {
+    id: 3,
+    type: 'Game',
+    tech: ['React', 'Redux', 'Sass', 'Phaser', 'Node.js', 'Express'],
+    title: "O'Survivors",
+    descriptionEn:
+      "O'Survivors is a 2D survival game where players must withstand increasingly powerful waves of monsters. ",
+		descriptionFr: "O'Survivors est un jeu de survie en 2D où les joueurs doivent résister à des vagues de monstres de plus en plus puissantes.",
+    image: '/projects/o-survivors.png',
+    codeLink: 'https://github.com/denispianelli/projet-tp-front',
+    demoLink: 'https://projet-tp-front.vercel.app/',
+  },
 ];
 
-module.exports = { projects };
