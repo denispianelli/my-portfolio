30781090c89f5372a8f5e62df49a6af973c82660
Denis Pianelli <denis.pianelli@gmail.com>
2024-06-01 10:35:29 +0200

    chore: Update NextIntlClientProvider usage in layout.tsx and add generateMetadata function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,12 @@ import { Poppins as FontSans } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { Header } from '@/components/page-header';
 import { Toaster } from '@/components/ui/toaster';
-
 import { ThemeProvider } from '@/components/theme-provider';
 import Footer from '@/components/footer';
 
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages } from 'next-intl/server';
+import { getTranslations } from 'next-intl/server';
 
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
@@ -18,6 +18,58 @@ const fontSans = FontSans({
   variable: '--font-sans',
   weight: ['400', '700'],
 });
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = await getTranslations({ locale, namespace: 'Metadata' });
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    keywords: [
+      'portfolio',
+      'développeur web',
+      'Fullstack',
+      'JavaScript',
+      'TypeScript',
+      'Next.js',
+      'React',
+      'Tailwind CSS',
+      'développeur frontend',
+      'développeur backend',
+      'développement web',
+      'applications web',
+      'sites web',
+      'web design',
+      'création de sites',
+      'web developer',
+      'full stack developer',
+      'web development',
+      'web applications',
+      'websites',
+      'website creation',
+      'programming',
+      'UI/UX',
+      'SEO',
+      'web technologies',
+      'JavaScript framework',
+      'programmation web',
+      'conception web',
+      'référencement',
+    ],
+    metadataBase: new URL('https://www.denispianelli.dev'),
+    alternate: {
+      canonical: '/',
+      languages: {
+        'en-US': '/en',
+        'fr-FR': '/fr',
+      },
+    },
+  };
+}
 
 export default async function RootLayout({
   children,
